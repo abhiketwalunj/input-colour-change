b3d63d4770d40f2bc90fe256128b0cd9cd9cc491
unknown <walunjabhiket@gmail.com>
2024-01-25 15:50:51 +0530

    input-colour-change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const ColorChanger = () => {
+  const colorArray = ['red', 'yellow', 'blue', 'green', 'brown', 'orange', 'gray', 'wheat', 'pink', 'purple', 'indigo', 'violet', 'skyblue','maroon'];
+  const [inputColor, setInputColor] = useState('');
+
+  const handleInputChange = (event) => {
+    const color = event.target.value.toLowerCase();
+    setInputColor(color);
+
+    if (colorArray.includes(color)) {
+      document.body.style.backgroundColor = color;
+    } else {
+      
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="color-container">
+      <input
+        type="text"
+        placeholder="Enter color name"
+        value={inputColor}
+        onChange={handleInputChange}
+      />
+
+      {colorArray.includes(inputColor) && (
+        <div className="selected-color" style={{ backgroundColor: inputColor }}>
+          Selected Color: {inputColor}
+        </div>
+      )}
+      {!colorArray.includes(inputColor) && (
+        <div className="error-message">
+          Color not found in the array.
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default ColorChanger;
